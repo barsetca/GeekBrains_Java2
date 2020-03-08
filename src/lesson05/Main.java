56ff95531d6b5c7c95038d994c57b92d75f3c570
Viktor Cherniak <barsetca@gmail.com>
2020-03-09 02:06:22 +0300

    lesson05 version01

---
+++ src/lesson05/Main.java
@@ -0,0 +1,9 @@
+package lesson05;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CheckMethodFillArrayRunTimeUtil.changeArrayWithOneThread();
+        CheckMethodFillArrayRunTimeUtil.changeArrayWithTwoThreads();
+    }
+}
