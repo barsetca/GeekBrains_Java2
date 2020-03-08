c8cdaf82847323acf5a4432b30b7f0f205541436
barsetca <56449299+barsetca@users.noreply.github.com>
2020-03-09 02:36:57 +0300

    Delete Main.java

--- src/lesson05/Main.java
+++
@@ -1,9 +0,0 @@
-package lesson05;
-
-public class Main {
-
-    public static void main(String[] args) {
-        CheckMethodFillArrayRunTimeUtil.changeArrayWithOneThread();
-        CheckMethodFillArrayRunTimeUtil.changeArrayWithTwoThreads();
-    }
-}
